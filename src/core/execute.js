646a5895aa83fbf2478f1b8a2c2fe0f3a48cf95e
Nick Gromyko <nxgr.dev@gmail.com>
2022-10-27 14:29:38 +0600

    fix(src/core/execute.js): matching arguments length

--- src/core/execute.js
+++ src/core/execute.js
@@ -5,13 +5,13 @@ const commands = require('./commands'),
 module.exports = async function (message, date) {
     const { token, arguments } = getCommand(message)
 
-    const command = commands[token]
+    const command = Object.values(commands).find(command => command.syntax === token)
 
     if (!command) {
         throw new CommandError(404, 'Unknown command')
     }
 
-    const requiredParametersLength = command.arguments.map(arg => arg.required).length
+    const requiredParametersLength = command.arguments.filter(arg => arg.required === true)
     if (requiredParametersLength > (arguments ? arguments.length : 0)) {
         throw new CommandError(400, `Command do not match parameters. Command must contain ${requiredParametersLength} arguments`)
     }
