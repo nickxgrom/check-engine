ea785527bc6c4c067ba6584244fb85f7110f0935
Nick Gromyko <nxgr.dev@gmail.com>
2022-10-30 22:57:06 +0600

    update(index.js): send message on error

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ require('dotenv').config()
 const app = require('express')(),
     bodyParser = require('body-parser'),
     router = require('./src/controller'),
-    CommandError = require("./src/core/CommandError"),
-    db = require('./src/utils/db')
-
+    CommandError = require('./src/core/CommandError'),
+    ServiceError = require('./src/utils/ServiceError'),
+    db = require('./src/utils/db'),
+    api = require('./src/utils/api')
 
 app.use(bodyParser.json())
 app.use(router)
 
 app.use(async function(err, req, res, next) {
-    if (err instanceof CommandError) {
-        console.log(err.message)
+    if (err instanceof CommandError || err instanceof ServiceError) {
+        const chatId = req.body.message.from.id
+        await api.sendMessage(err.message, chatId)
         res.sendStatus(200)
     } else next(err)
 })
