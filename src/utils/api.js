c2d3b14c9d85b8e538e0f83ff3770f4190ffc096
Nick Gromyko <nxgr.dev@gmail.com>
2022-10-31 21:51:11 +0600

    update(src/utils/api.js): sending text as a markdown

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,8 @@ module.exports = {
     sendMessage: async (message, chatId) => {
         await axios.post(`${baseUrl}/${process.env.BOT_TOKEN}/sendMessage`, {
             chat_id: chatId,
-            text: message
+            text: message,
+            parse_mode: 'markdown'
         })
     }
 }
